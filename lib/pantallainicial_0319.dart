fb9cdc6f9026a3d49c12b73e063f55fb36b81b88
BeltranJ128 <143763139+BeltranJ128@users.noreply.github.com>
2024-03-22 12:33:22 -0600

    Update pantallainicial_0319.dart

--- lib/pantallainicial_0319.dart
+++ lib/pantallainicial_0319.dart
@@ -8,7 +8,7 @@ class Pantallainicial_0319 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Pantalla Inicial Beltrán 0319"),
+        title: const Text("Pantalla Inicial Beltrán 0319"),
         backgroundColor: Colors.lightGreen,
       ),
       body: Center(
@@ -19,13 +19,97 @@ class Pantallainicial_0319 extends StatelessWidget {
               onPressed: () {
                 Navigator.pushNamed(context, "/pantalla1_0319");
               },
-              child: Text("Mover a pantalla1"),
+              child: const Text("Mover a pantalla1"),
             ),
             ElevatedButton(
               onPressed: () {
                 Navigator.pushNamed(context, "/pantalla2_0319");
               },
-              child: Text("Mover a pantalla2"),
+              child: const Text("Mover a pantalla2"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/pantalla3_0319");
+              },
+              child: const Text("Mover a pantalla3"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/pantalla4_0319");
+              },
+              child: const Text("Mover a pantalla4"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/pantalla5_0319");
+              },
+              child: const Text("Mover a pantalla5"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/pantalla6_0319");
+              },
+              child: const Text("Mover a pantalla6"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/pantalla7_0319");
+              },
+              child: const Text("Mover a pantalla7"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/pantalla8_0319");
+              },
+              child: const Text("Mover a pantalla8"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/pantalla9_0319");
+              },
+              child: const Text("Mover a pantalla9"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/pantalla10_0319");
+              },
+              child: const Text("Mover a pantalla10"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/pantalla11_0319");
+              },
+              child: const Text("Mover a pantalla11"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/pantalla12_0319");
+              },
+              child: const Text("Mover a pantalla12"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/pantalla13_0319");
+              },
+              child: const Text("Mover a pantalla13"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/pantalla14_0319");
+              },
+              child: const Text("Mover a pantalla14"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/pantalla15_0319");
+              },
+              child: const Text("Mover a pantalla15"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, "/pantalla16_0319");
+              },
+              child: const Text("Mover a pantalla16"),
             ),
           ], //niños widget
         ),
