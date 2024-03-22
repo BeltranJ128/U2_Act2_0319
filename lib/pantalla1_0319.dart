2adb5e8347c0e1fa6da46eee9e2e611f1cc707f3
BeltranJ128 <143763139+BeltranJ128@users.noreply.github.com>
2024-03-22 12:32:39 -0600

    Update pantalla1_0319.dart

--- lib/pantalla1_0319.dart
+++ lib/pantalla1_0319.dart
@@ -8,14 +8,44 @@ class Pantalla1_0319 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Pantalla1 Beltrán 0319"),
+        title: const Text("Pantalla1 Beltrán 0319"),
         backgroundColor: Colors.pinkAccent,
       ),
       body: Center(
         child: Column(
           children: [
+            const Text(
+              'Jose Carlos Beltran Gamez',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.deepPurple,
+              ),
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                margin: const EdgeInsets.only(top: 20),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: Colors.deepPurple,
+                    width: 10,
+                  ),
+                ),
+                width: 280,
+                height: 280,
+                alignment: Alignment.center,
+                child: const Text(
+                  'J',
+                  style: TextStyle(
+                    fontSize: 180,
+                    color: Colors.deepPurple,
+                  ),
+                ),
+              ),
+            ),
             Container(
-              child: Text("Beltrán EJEMPLO"),
+              child: Text("Aterrizaje Mat. 21308051280319"),
             )
           ], //niños
         ),
